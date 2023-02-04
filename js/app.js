26b19cbb557aeae6c017a8d9143cfaa1f43eadc6
Kavvannah <103372963+Kavvannah@users.noreply.github.com>
2023-02-03 22:22:05 -0800

    :heavy_plus_sign: added new todo event listener

---
+++ js/app.js
@@ -0,0 +1,10 @@
+const addForm = document.querySelector('.add');
+
+// html template
+
+//add new todo
+addForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const todo = addForm.add.value.trim(); //trim cuts out uneccessary white space
+    
+});
