ca404b426de1b0835d107dd0214dbcfe9b9fd80d
Kavvannah <103372963+Kavvannah@users.noreply.github.com>
2023-02-03 22:32:06 -0800

    :heavy_plus_sign: html template when adding todo

--- js/app.js
+++ js/app.js
@@ -1,10 +1,20 @@
 const addForm = document.querySelector('.add');
-
+const list = document.querySelector('.todos');
 // html template
+const generTemplate = todo => {
+    
+    const html = `
+        <li class="list-group-item d-flex justify-content-between align-items-center">
+            <span>${todo}</span>
+            <i class="bi bi-trash delete"></i>
+        </li>
+        `;
 
+};
 //add new todo
 addForm.addEventListener('submit', e => {
     e.preventDefault();
     const todo = addForm.add.value.trim(); //trim cuts out uneccessary white space
     
+    generTemplate(todo);
 });
