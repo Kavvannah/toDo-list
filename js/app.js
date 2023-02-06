e0a208c62050ab8ce1749a04f45d8b0c08b82fe2
Kavvannah <103372963+Kavvannah@users.noreply.github.com>
2023-02-05 23:42:52 -0800

    :heavy_plus_signnegating to return array

--- js/app.js
+++ js/app.js
@@ -40,7 +40,7 @@ list.addEventListener('click', e => {
 const filterTodos = (term) => {  // outside of event in order to use it elsewhere
     Array.from(list.children)
         .filter(todo => {
-            return todo.textContent.includes(term); //searches term to see if it passes check
+            return !todo.textContent.includes(term); //searches term to see if it passes check
         })
 };
 
